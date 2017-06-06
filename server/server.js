976d59a244319a46d22b3eec29527eb6c15a5b48
Shock Liang <shockwater@gmail.com>
2017-06-06 21:50:29 +0800

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -64,6 +65,35 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(404).send();
     });
+});
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // To mapping which properties that want to updated.
+    var body = _.pick(req.body, ['text', 'complete']);
+    
+    if(!ObjectID.isValid(id)) {
+            return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.complete) && body.complete) {
+        body.completeAt = new Date().getTime();
+    } else {
+        body.complete = false;
+        body.completeAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(prot, () => {
