51c834610f4b4bab30a9558bafcd363709957e68
Shock Liang <shockwater@gmail.com>
2017-06-14 22:25:08 +0800

    Add user login route

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,29 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+    var decodedPassword;
+    
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject('No such user');
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password,user.password, (err, res) => {
+                if(res) {
+                    return resolve(user);
+                } else {
+                    return reject('Password not correctly');
+                }
+            });
+        });
+    });
+
+    
+};
+
 UserSchema.pre('save', function(next) {
     var user = this;
     var hashedPassword = '';
