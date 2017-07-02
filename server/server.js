bd2c4188595f16fc0d2216b4d3ae25ba5a47416e
Shock Liang <shockwater@gmail.com>
2017-07-02 15:27:52 +0800

    Using async/await to refactories

--- server/server.js
+++ server/server.js
@@ -16,76 +16,78 @@ const prot = process.env.PORT;
 app.use(bodyParser.json());
 
 // POST /todos
-app.post('/todos',authenticate, (req, res) => {
+app.post('/todos',authenticate, async (req, res) => {
     var todo = new Todo({
         text:req.body.text,
         _creator: req.user._id
     });
 
-    todo.save().then((doc) => {
+    try {
+        const doc = await todo.save();
         res.status(200).send(doc);
-    }, (e) => {
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
 // GET /todos/
-app.get('/todos', authenticate, (req, res) => {
-    Todo.find({
-        _creator: req.user._id
-    }).then((todos) => {
+app.get('/todos', authenticate, async (req, res) => {
+    try {
+        const todos = await Todo.find({
+                        _creator: req.user._id
+                    });
         res.send({todos});
-    }, (err) => {
+    } catch (e) {
         res.status(400).send(err);
-    });
+    }
 });
 
 // GET /todos/123456
-app.get('/todos/:id', authenticate, (req, res) => {
+app.get('/todos/:id', authenticate, async (req, res) => {
     var id = req.params.id;
     // Valid id
     if(!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findOne({
-            _id: id,
-            _creator: req.user._id
-        }).then((todo) => {
+    try {
+        const todo = await Todo.findOne({
+                        _id: id,
+                        _creator: req.user._id
+                    });
         todo ? res.send({todo}) : res.status(404).send();
-    }).catch((e) => {
-        console.log(e);
+    } catch (e) {
         res.status(404).send();
-    });
-
+    }
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', authenticate, (req, res) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
     var id = req.params.id;
     // Valid id
     if(!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+    try {
+        const todo = await Todo.findOneAndRemove({
+                        _id: id,
+                        _creator: req.user._id
+                    });
         todo ? res.send({todo}) : res.status(404).send();
-    }).catch((e) => {
+    } catch (e) {
         res.status(404).send();
-    });
+    }
 });
 
 // PATCH /todos/:id
-app.patch('/todos/:id', authenticate, (req, res) => {
+app.patch('/todos/:id', authenticate, async (req, res) => {
     var id = req.params.id;
     // To mapping which properties that want to updated.
     var body = _.pick(req.body, ['text', 'complete']);
     
     if(!ObjectID.isValid(id)) {
-            return res.status(404).send();
+        return res.status(404).send();
     }
 
     if(_.isBoolean(body.complete) && body.complete) {
@@ -95,33 +97,32 @@ app.patch('/todos/:id', authenticate, (req, res) => {
         body.completeAt = null;
     }
 
-    Todo.findOneAndUpdate({
+    try {
+        const todo = await Todo.findOneAndUpdate({
             _id: id,
             _creator: req.user._id
-        }, {$set: body}, {new: true}).then((todo) => {
+        }, {$set: body}, {new: true});
+
         if(!todo) {
             return res.status(404).send();
         }
-
         res.send({todo});
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send();
-    });
-
+    }
 });
 
 // POST /users
-app.post('/users/',(req, res) => {
+app.post('/users/', async (req, res) => {
     var body = _.pick(req.body, ['name', 'email','password']);
     var user = new User(body);
-
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
+    try{
+        await user.save();
+        const token = user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
 // GET /users/me
